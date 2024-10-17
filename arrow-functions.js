75edbf9afd0c7193cbf41820f31997a81a5994ee
John Gardner <john.gardner@msn.com>
2024-10-17 11:32:33 +0000

    inital commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -13,8 +13,29 @@ console.log(sum);
 
 // Arrow Function With Parameters
 
+const test1 = (a ,b) => {return a+b;}
+let sum1 = test1(2, 5);
+console.log(sum1);
+
 // Single Line Arrow Function With Parameters
+// const test2 = (a ,b) => (a+b); //also valid
+const test2 = (a ,b) => a+b;
+let sum2 = test1(2, 6);
+console.log(sum2);
 
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething('world');
+
+const saySomething1 = () => console.log('hello');
+saySomething1();
+
 // Returning Multiple Lines
+const saySomething2 = () => (
+`<p>
+    this is a multiple line string
+</p>`
+)
+
+console.log(saySomething2());
